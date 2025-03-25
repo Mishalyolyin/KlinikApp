a58a3684e97f8c790d0ec2a5bb26e1d57a4cb2f9
Mishalyolyin <111202214691@mhs.dinus.ac.id>
2025-03-25 10:54:39 +0700

    commit pertama

---
+++ database/migrations/2025_03_25_033418_create_periksa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('periksa', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_pasien');
+            $table->unsignedBigInteger('id_dokter');
+            $table->dateTime('tgl_periksa');
+            $table->text('catatan')->nullable();
+            $table->integer('biaya_periksa');
+            $table->timestamps();
+
+            $table->foreign('id_pasien')->references('id')->on('users');
+            $table->foreign('id_dokter')->references('id')->on('users');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('periksa');
+    }
+};
